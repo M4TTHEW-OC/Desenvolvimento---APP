84a56bb3e3305653ad904ed2669c1dcd56e739a3
MATHEUS DE OLIVEIRA CAVALCANTE <99699238+M4TTHEW-OC@users.noreply.github.com>
2024-09-26 23:00:35 -0300

    Delete ex.1 directory

--- ex.1/ex -1.js
+++
@@ -1,6 +0,0 @@
-// Solicita o nome e o sobrenome do usuário
-let nome = prompt("Por favor, digite seu nome:");
-let sobrenome = prompt("Agora, digite seu sobrenome:");
-
-// Exibe a mensagem de boas-vindas
-alert("Bem-vindo(a), " + nome + " " + sobrenome + "!");
